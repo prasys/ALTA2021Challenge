abbf110b5a607ab6f26d23aeed26c5302ce36ef9
prasys <pradeesh@prasys.info>
2021-07-30 10:24:56 +1200

    fixed the logic for cleanText

--- preprocess.py
+++ preprocess.py
@@ -9,7 +9,29 @@ from cleantext import clean
 
 
 
-def clean_text()
+def clean_text(text):
+    return(clean("some input",
+    fix_unicode=True,               # fix various unicode errors
+    to_ascii=True,                  # transliterate to closest ASCII representation
+    lower=True,                     # lowercase text
+    no_line_breaks=False,           # fully strip line breaks as opposed to only normalizing them
+    no_urls=False,                  # replace all URLs with a special token
+    no_emails=False,                # replace all email addresses with a special token
+    no_phone_numbers=False,         # replace all phone numbers with a special token
+    no_numbers=True,               # replace all numbers with a special token
+    no_digits=True,                # replace all digits with a special token
+    no_currency_symbols=False,      # replace all currency symbols with a special token
+    no_punct=False,                 # remove punctuations
+    replace_with_punct="",          # instead of removing punctuations you may replace them
+    replace_with_url="<URL>",
+    replace_with_email="<EMAIL>",
+    replace_with_phone_number="<PHONE>",
+    replace_with_number="<NUMBER>",
+    replace_with_digit="0",
+    replace_with_currency_symbol="<CUR>",
+    lang="en"
+    ))
+
 
 def parse_text(text, patterns=None):
     """
@@ -35,16 +57,20 @@ def parse_text(text, patterns=None):
         final_text = re.sub(pattern, repl, final_text)
     return final_text.strip()
 
-def parseAbstracts(fileName):
+def parseAbstracts(fileName,cleanText=True):
     s = ""
     texts = []
     abstracts = loadXMLcontents(fileName)
     if len(abstracts) == 1:
         s = parse_text(abstracts[0])
+        if cleanText:
+            s = clean_text(s)
     else:
         for abstract in abstracts:
             texts.append(parse_text(abstract))
             s = ''.join(texts)
+            if cleanText:
+                s = clean_text(s)
     return s
 
 def loadXMLcontents(fileName,tag="abstracttext"):
@@ -54,17 +80,17 @@ def loadXMLcontents(fileName,tag="abstracttext"):
     content = soup.find_all(tag)
     return content
 
-def loadFileAndParse(filename,XMLFolderPath,truth,docCollections):
+def loadFileAndParse(filename,XMLFolderPath,truth,docCollections,cleanText):
     file1 = open(filename, 'r')
     for line in file1:
         temp = line.split()
         if (len(temp)-2) == 1: #if we only have 1 document collection
             truth.append(temp[1]) #append the grade
-            docCollections.append(parseAbstracts(XMLFolderPath+temp[2]+".xml")) #parseXML
+            docCollections.append(parseAbstracts(XMLFolderPath+temp[2]+".xml"),cleanText) #parseXML
         elif (len(temp)-2) > 1:
                 for count in range(2,len(temp)):
                     truth.append(temp[1])
-                    docCollections.append(parseAbstracts(XMLFolderPath+temp[count]+".xml")) #parseXML  
+                    docCollections.append(parseAbstracts(XMLFolderPath+temp[count]+".xml"),cleanText) #parseXML  
     file1.close()
 
 
@@ -81,11 +107,12 @@ def main():
     parser.add_argument('--file', default='trainset.txt', type=str)
     parser.add_argument('--XMLdir', default='/trainset/', type=str)
     parser.add_argument('--processedFile', default='output.h5', type=str)
+    parser.add_argument('--cleanText', default=True, type=bool)
     opt = parser.parse_args()
     xmlPath = os.path.abspath(os.path.dirname(os.path.abspath(__file__))) + opt.XMLdir
     docCollections = []
     truthLabels = []
-    loadFileAndParse(opt.file,xmlPath,truthLabels,docCollections)
+    loadFileAndParse(opt.file,xmlPath,truthLabels,docCollections,opt.cleanText)
     createAndSaveDataFrame(truthLabels,docCollections,opt.processedFile)
 
 
