740bd150543e5d24dd4c426e665a9206a7214f35
prasys <pradeesh@prasys.info>
2021-07-29 17:29:56 +1200

    Update trainModel.py

--- trainModel.py
+++ trainModel.py
@@ -2,6 +2,8 @@ from simpletransformers.classification import ClassificationModel, Classificatio
 import pandas as pd
 import logging
 import pickle5 as pickle
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_recall_fscore_support
 
 
 logging.basicConfig(level=logging.INFO)
@@ -16,16 +18,18 @@ eval_df = pd.read_pickle('dev.h5')
 
 # Optional model configuration
 model_args = ClassificationArgs()
-model_args.num_train_epochs=15
-model_args.labels_list = ["A", "B","C"]
+model_args.reprocess_input_data = True
+model_args.overwrite_output_dir = True
+model_args.use_multiprocessing = True
+model_args.num_train_epochs=4
 
 # Create a ClassificationModel
 model = ClassificationModel(
     'bert',
-    'jambo/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext-finetuned-renet',
+    'microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext',
     num_labels=3,
     args=model_args,
-    weight=[0,0.5,1]
+    weight=[0.313,0.460,0.227]
 ) 
 
 # Train the model
@@ -33,4 +37,8 @@ model.train_model(train_df)
 
 # Evaluate the model
 result, model_outputs, wrong_predictions = model.eval_model(eval_df)
+print(result)
+print("macro f1 score " + precision_recall_fscore_support(eval_df['labels'],model_outputs),average='macro')
+print("micro f1 score " + precision_recall_fscore_support(eval_df['labels'],model_outputs),average='micro')
+print(wrong_predictions)
 
