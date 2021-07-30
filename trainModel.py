eca5c02ecbc45c61b0d2334bd2b68a535bb102f7
prasys <pradeesh@prasys.info>
2021-07-30 16:05:15 +1200

    changes to preprocessor to include export to CSV

--- trainModel.py
+++ trainModel.py
@@ -5,7 +5,18 @@ import pickle5 as pickle
 from sklearn.metrics import f1_score
 from sklearn.metrics import precision_recall_fscore_support
 import sklearn
+import argparse
+import os,sys
+import wandb
 
+
+eval_metrics = {
+    "macroF1" : calcMacroF1,
+    "microF1" : calcMacroRecall,
+    "macroRecall" : calcMacroRecall,
+    "microRecall" : calcMicroRecall,
+    "Acc" : calcAccScore
+}
 
 def calcMicroF1(trueP,falseP):
     """[Caclulates MicroF1 scores using Sklearn implementation]
@@ -36,41 +47,92 @@ transformers_logger = logging.getLogger("transformers")
 transformers_logger.setLevel(logging.WARNING)
 
 # Preparing train data
-train_df = pd.read_pickle('train.h5')
+train_df = pd.read_pickle('train3.h5')
 
 # Preparing eval data
-eval_df = pd.read_pickle('dev.h5')
+eval_df = pd.read_pickle('dev3.h5')
 
 # Optional model configuration
 model_args = ClassificationArgs()
-model_args.reprocess_input_data = False
+model_args.reprocess_input_data = True
 model_args.overwrite_output_dir = True
 model_args.use_multiprocessing = True
 model_args.num_train_epochs=5
 model_args.use_early_stopping= True
 model_args.do_lower_case = True
-model_args.max_seq_length = 500
-model_args.best_model_dir = "output/best_bio"
-model_args.train_batch_size = 32
+model_args.early_stopping_delta = 0.01
+model_args.early_stopping_metric = "mcc"
+model_args.early_stopping_metric_minimize = False
+model_args.early_stopping_patience = 5
+model_args.evaluate_during_training_steps = 1000
+model_args.max_seq_length = 128
+model_args.best_model_dir = "output/best_core3"
+model_args.train_batch_size = 16
+model_args.eval_batch_size = 16
+model_args.sliding_window = False
+model_args.save_steps = -1
+model_args.special_tokens_list = ["[NUMBER]","[DIGIT]","[PHONE]","[URL]","[EMAIL]"]
+model_args.adam_epsilon = 1e-8 #1e-8 is default
+model_args.polynomial_decay_schedule_lr_end = 1e-7 # 1e-7 is default
+model_args.scheduler = "cosine_schedule_with_warmup"
+model_args.wandb_project = "alta2021"
+model_args.wandb_kwargs = {'name': 'optimize'}
+
+# Doing sweep to check our optimisation 
+sweep_config = {
+    "method": "bayes",  # grid, random
+    "metric": {"name": "train_loss", "goal": "minimize"},
+    "parameters": {
+        "num_train_epochs": {"values": [2, 3, 5]},
+        "learning_rate": {"min": 5e-5, "max": 4e-4},
+    },
+}
+
+sweep_id = wandb.sweep(sweep_config, project="alta2021")
 
 
-# Create a ClassificationModel
-model = ClassificationModel(
-    'bert',
-    'microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext',
-    num_labels=3,
-    args=model_args,
-    weight=[0.313,0.460,0.227]
-) 
+def train():
+    wandb.init()
+    model = ClassificationModel(
+        'bert',
+        # '',
+        'microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract',
+        num_labels=3,
+        args=model_args,
+        sweep_config=wandb.config,
+        weight=[1.06,0.723,1.47]
+    )
+    model.train_model(train_df,output_dir='output/best_core3')
+    result, model_outputs, wrong_predictions = model.eval_model(eval_df,**eval_metrics)
+    wandb.join()
 
+wandb.agent(sweep_id, train)
+
+9
 # Train the model
-model.train_model(train_df,output_dir='output/best_bio')
+
 
 # model = ClassificationModel(
 #     "bert", "/data/pradeesh/ALTA2021Challenge/output/best_bio"
 # )
 
 # Evaluate the model
-result, model_outputs, wrong_predictions = model.eval_model(eval_df,micF1=calcMicroF1,macroF1=calcMacroF1,macroRecall=calcMacroRecall,microRecall=calcMicroRecall,acc=calcAccScore)
-print(result)
+#print(result)
+
+
+# def main():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--model', default='trainset.txt', type=str)
+#     parser.add_argument('--modelPath', default='/trainset/', type=str)
+#     parser.add_argument('--load', default='output.h5', type=bool)
+#     parser.add_argument('--cleanText', default=True, type=bool)
+#     opt = parser.parse_args()
+#     xmlPath = os.path.abspath(os.path.dirname(os.path.abspath(__file__))) + opt.XMLdir
+#     docCollections = []
+#     truthLabels = []
+#     loadFileAndParse(opt.file,xmlPath,truthLabels,docCollections,opt.cleanText)
+#     createAndSaveDataFrame(truthLabels,docCollections,opt.processedFile)
+
+# if __name__ == '__main__':
+#     main()
 
